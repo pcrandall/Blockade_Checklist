6f220829b7526ca6f03d1d4b107d304b6089869c
pcrandall <crandall.phillip@gmail.com>
2019-12-21 17:16:47 -0600

    Finished.

--- js/main.js
+++ js/main.js
@@ -78,10 +78,10 @@ window.addEventListener('load', function () {
 
     /* set up table headers*/
     var L = 0;
-    json.forEach(function (r) {
-      if (L < r.length) L = r.length;
+    json.forEach(function (row) {
+      if (L < row.length) L = row.length;
     });
-    console.log(L);
+    //console.log(L);
 
     //Sort by floor number NRA3204X43200Y05Z12 where "4" is index 6
     json.forEach((loc, index) => {
@@ -90,23 +90,29 @@ window.addEventListener('load', function () {
         var nav1 = a[0][3] + a[0][4];
         var nav2 = b[0][3] + b[0][4];
         //if on the same floor
-        if (a[0][6] == b[0][6]){
-          return nav1 > nav2 ? -1 : nav2 > nav1 ? 1: 0;
-        }else{
+        if (a[0][6] == b[0][6]) {
+          return nav1 > nav2 ? -1 : nav2 > nav1 ? 1 : 0;
+        } else {
           return a[0][6] > b[0][6] ? -1 : b[0][6] > a[0][6] ? 1 : 0;
         }
       }
-      
+
       json.sort(sortThings);
     });
 
+    //Check the lenght, make sure we have at least three columns for the sheet.
+    if (json[0].length < 2) {
+      json.unshift(["STOLOC", "LU", "Verified LU"]);
+    }
+
     /* load data */
     cdg.data = json;
+
     //first row is the header, save the first row for later use. 
     firstRow = json[0];
-    for (var i = 0; i < L; i++) {
+    for (var i = 0; L < 3 ? i < 3 : i < L; i++) {
       cdg.schema[i].title = cdg.data[0][i];
-      console.log(cdg.schema[i].title);
+      //console.log(cdg.schema[i].title);
     }
 
     cdg.attributes.columnHeaderClickBehavior = 'select';
@@ -118,21 +124,14 @@ window.addEventListener('load', function () {
     cdg.addEventListener('click', function (e) {
       if (!e.cell || e.cell.columnIndex !== 0) {
         parseData(e.cell.data[0]);
-        console.log(cdg.data[0]);
+        //console.log(cdg.data[0]);
         return;
       }
       parseData(e.cell.value);
     });
 
-    cdg.addEventListener('keydown', function (e) {
-      if (!e.cell || e.cell.columnIndex !== 0) {
-        console.log(e.cell);
-        console.log(e.cell.selected);
-        console.log(cdg.activeCell)
-        return;
-      }
-      parseData(e.cell.value);
-    });
+    //focus the bottom of the screen after the sheet loads. 
+    window.scrollTo(0, document.body.scrollHeight);
 
   };
   //Listen for the save button to be pressed
@@ -148,20 +147,28 @@ window.addEventListener('load', function () {
     var wb = XLSX.utils.book_new();
     var ws = XLSX.utils.aoa_to_sheet(cdg.data);
     //set the column width
-    var wscols =[
-      {wch: 25}, //chars
-      {wpx: 125}, //pixels
-      {width: 20} // max digit width
+    var wscols = [{
+        wch: 25
+      }, //chars
+      {
+        wpx: 125
+      }, //pixels
+      {
+        width: 20
+      } // max digit width
     ];
-    ws['!cols'] = wscols; 
+    ws['!cols'] = wscols;
 
     XLSX.utils.book_append_sheet(wb, ws)
     var d = new Date();
+    /*
     let m = d.toLocaleString('default', {
       month: 'long'
     });
-    d = d.toJSON().slice(0, 5);
-    var filename = d + m + ' Blockade Checklist Complete.xlsx'
+    */
+
+    d = d.toJSON().slice(0, 10);
+    var filename = d + '_Blockade Checklist Complete.xlsx'
     var buffer = XLSX.writeFile(wb, filename)
   };
   /** Drop it like it's hot **/
